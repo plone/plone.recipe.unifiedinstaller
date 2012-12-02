c8c65d309683f038e6dfe59278d99ec90a99b22e
Steve McMahon <steve@dcn.org>
2012-12-02 13:19:04 -0800

    Rework for 4.3+

--- src/plone/recipe/unifiedinstaller/__init__.py
+++ src/plone/recipe/unifiedinstaller/__init__.py
@@ -1,4 +1,4 @@
-import os, re, shutil, logging
+import os
 import zc.buildout
 import zc.recipe.egg
 
@@ -15,63 +15,72 @@ class Recipe:
 
         # logging.getLogger(self.name).info(buildout['buildout'])
 
-        options['scripts'] = '' # suppress script generation.
-        options['location'] = buildout['buildout']['directory']
-        options['bin-directory'] = buildout['buildout']['bin-directory']
-        options['parts-directory'] = buildout['buildout']['parts-directory']
-        
-        options['scripts'] = '' # suppress script generation.
+        self._location = buildout['buildout']['directory']
+        self._bin_directory = buildout['buildout']['bin-directory']
+        self._parts_directory = buildout['buildout']['parts-directory']
+        self._shell_command = options.get('shell-command', '/bin/sh')
+        self._sudo_command = options.get('sudo-command', 'SUDOXXX')
+        self._start_command = options.get('start-command', 'plonectl start')
+        self._stop_command = options.get('stop-command', 'plonectl stop')
+        self._user = options.get('user', '')
+        self._effective_user = options.get('effective-user', '')
+        self._buildout_user = options.get('buildout-user', '')
+        self._need_sudo = options.get('need-sudo', 'false').lower() in ('true', 'yes', 'on')
 
-        options.setdefault('shell-command', '/bin/sh')
-        options.setdefault('sudo-command', '')
+        # options['scripts'] = ''  # suppress script generation.
+
+        file_storage = options.get('file-storage', os.path.join(self._location, 'var', 'filestorage', 'Data.fs'))
+        file_storage = os.path.join(self._location, file_storage)
+        self._fileStorage = file_storage
+        self._fileStorageDir = os.path.dirname(file_storage)
+
+    def install(self):
+        options = self.options
+
+        buildout_parts = self.buildout['buildout'].get('parts', '').split()
 
         zeoserver = options.get('zeoserver')
         if zeoserver is None:
             # look for server definitions in the buildout
             servers = \
-                [part for part in buildout.keys() 
-                 if buildout[part].get('recipe', '') in ('plone.recipe.zope2zeoserver','plone.recipe.zeoserver')]
+                [part for part in buildout_parts
+                 if self.buildout[part].get('recipe', '')
+                    in ('plone.recipe.zope2zeoserver', 'plone.recipe.zeoserver')
+                ]
             if len(servers) == 1:
-                options['zeoserver'] = servers[0]
+                self._zeoserver = servers[0]
 
         clients = options.get('clients')
         if clients is None:
             # look for client definitions in the buildout
-            clients = [part for part in buildout.keys() if buildout[part].get('recipe', '') == 'plone.recipe.zope2instance']
-            options['clients'] = '\n'.join( clients )
-
-        options.setdefault('start-command', 'plonectl start')
-        options.setdefault('stop-command', 'plonectl stop')
+            clients = [
+                part for part in buildout_parts
+                if self.buildout[part].get('recipe', '') == 'plone.recipe.zope2instance'
+                ]
+            self._clients = '\n'.join(clients)
 
         # let's find a client port that we can use in the README
-        client_ports = [buildout[part].get('http-address') for part in buildout.keys() if buildout[part].get('recipe', '') == 'plone.recipe.zope2instance']
+        client_ports = [
+            self.buildout[part].get('http-address')
+            for part in clients
+            ]
         if client_ports and client_ports[0]:
-            options.setdefault('primary-port', client_ports[0])
+            self._primary_port = client_ports[0]
         else:
-            options.setdefault('primary-port', 'port')
+            self._primary_port = options.get('port')
 
-        file_storage = options.get('file-storage', os.path.join(options['location'], 'var', 'filestorage', 'Data.fs'))
-        file_storage = os.path.join(options['location'], file_storage)
-        options['fileStorage'] = file_storage
-        options['fileStorageDir'] = os.path.dirname(file_storage)
-
-
-    def install(self):
-        options = self.options
-
-        paths = [ self.writeTemplate('admin_text', 'adminPassword.txt') ]
+        paths = [self.writeTemplate('admin_text', 'adminPassword.txt')]
         os.chmod(paths[0], 0600)
 
-        # self.writeTemplate('readme_text', 'README.txt')
-        # paths.append('README.txt')
-        
-        self.writeTemplate('parts_readme_text', os.path.join(options['location'], options['parts-directory'], 'README.txt'))
-        paths.append('%s/README.txt' % options['parts-directory'])
+        self.writeTemplate('parts_readme_text',
+            os.path.join(self._location, self._parts_directory, 'README.txt')
+            )
+        paths.append('%s/README.txt' % self._parts_directory)
 
         self.writeTemplate('readme_html', 'README.html')
         paths.append('README.html')
-        
-        if options.has_key('zeoserver') and options.has_key('clients'):
+
+        if self._zeoserver and self._clients in options:
             paths = paths + self.writeZeoScripts()
 
         requirements, ws = self.egg.working_set(['plone.recipe.unifiedinstaller'])
@@ -79,65 +88,46 @@ class Recipe:
             [('plonectl',
                 'plone.recipe.unifiedinstaller.ctl', 'main')],
             ws, options['executable'], options['bin-directory'],
-            extra_paths = [],
-            arguments = ("""server='%s', clients=%s, location=r'%s', binDirectory=r'%s', fileStorage=r'%s'""" % \
-                    (options.get('zeoserver',''),
-                     options.get('clients','').split(),
-                     options['location'],
-                     options['bin-directory'],
-                     options['fileStorage'])
+            extra_paths=[],
+            arguments=(
+                """server='%s', clients=%s, location=r'%s', binDirectory=r'%s', fileStorage=r'%s'""" % \
+                    (self._zeoserver,
+                     self._clients.split(),
+                     self._location,
+                     self._bin_directory,
+                     self._fileStorage)
                 ),
             )
 
         return paths
 
-
     def update(self):
         return self.install()
 
-
     def writeTemplate(self, template, filename):
-        options = self.options        
-        script = read(template) % dict(bin_dir = options['bin-directory'],
-                                      location = options['location'],
-                                      zeoserver = options.get('zeoserver', ''),
-                                      clients = ' '.join(options.get('clients', '').split()),
-                                      shell_cmd = options['shell-command'],
-                                      start_cmd = options['start-command'],
-                                      stop_cmd = options['stop-command'],
-                                      password = options['user'].split(':')[1],
-                                      user = options['user'].split(':')[0],
-                                      port = options['primary-port'],
-                                      sudo_cmd = options['sudo-command'],
-                                      file_storage = options['fileStorage'],
-                                      file_storage_dir = options['fileStorageDir'],)
-        
+        sudo_eu = ''
+        sudo_bu = ''
+        if self._need_sudo:
+            sudo_eu = "sudo -u %s" % self._effective_user
+            sudo_bu = "sudo -u %s" % self._buildout_user
+        options = self.options
+        script = read(template) % dict(
+          bin_dir=self._bin_directory,
+          location=self._location,
+          zeoserver=options.get('zeoserver', ''),
+          clients=' '.join(options.get('clients', '').split()),
+          shell_cmd=self._shell_command,
+          start_cmd=self._start_command,
+          stop_cmd=self._stop_command,
+          password=self._user.split(':')[1],
+          user=self._user.split(':')[0],
+          port=self._primary_port,
+          sudo_effective_user=sudo_eu,
+          sudo_buildout_user=sudo_bu,
+          file_storage=self._fileStorage,
+          file_storage_dir=self._fileStorageDir,
+          )
+
         open(filename, 'w').write(script)
 
         return filename
-        
-
-    def writeScript(self, template, filename):
-        options = self.options
-        
-        script_path = os.path.join(options['bin-directory'], filename)
-        self.writeTemplate(template, script_path)
-        os.chmod(script_path, 0755)
-        return script_path
-
-
-    def writeZeoScripts(self):
-        return [
-            self.writeScript('startcluster_template', 'startcluster.sh'),
-            self.writeScript('shutdown_template', 'shutdowncluster.sh'),
-            self.writeScript('restart_template', 'restartcluster.sh'),
-            self.writeScript('restart_clients_template', 'restartclients.sh'),
-            self.writeScript('status_template', 'clusterstatus.sh'),
-            # self.writeScript('plonectl_template', 'plonectl'),
-        ]
-
-    # def writeClientScripts(self):
-    #     return [
-    #         self.writeScript('mkPloneSite.py', 'mkPloneSite.py'),
-    #         # self.writeScript('plonectl_template', 'plonectl'),
-    #     ]
